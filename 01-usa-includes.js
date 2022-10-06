0001f926a851bc56b5a6ff685276baf2634912d8
Adolfo Gomez <agomezligero@yahoo.es>
2022-10-06 12:23:35 +0200

    first commit

---
+++ 01-usa-includes.js
@@ -0,0 +1,5 @@
+// **Iteración #1: Usa includes**
+
+// Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra "Camiseta".
+
+// Usa la función .***includes*** de javascript.
